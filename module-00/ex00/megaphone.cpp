6bad0102a41efdb7ef928cd15ece546dbab37026
Alienxbe <martin.ykman@gmail.com>
2025-06-15 15:19:12 +0200

    Module 00: Starting

---
+++ module-00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marykman <marykman@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/08 17:49:20 by marykman          #+#    #+#             */
+/*   Updated: 2025/06/08 20:12:30 by marykman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+		for (int i = 1; i < argc; i++)
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+	std::cout << std::endl;
+	return (0);
+}
