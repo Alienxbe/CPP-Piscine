618d46ddfb13c078d5720101beedf53992939403
Mykman <martin.ykman@gmail.com>
2025-11-05 05:10:30 +0100

    Module-02 ex02 + additional testing to main function

--- module-02/ex02/main.cpp
+++ module-02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: marykman <marykman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 02:16:25 by marykman          #+#    #+#             */
-/*   Updated: 2025/11/05 04:57:51 by marykman         ###   ########.fr       */
+/*   Updated: 2025/11/05 05:08:50 by marykman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,50 @@
 
 int main(void)
 {
-	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	{
+		std::cout << "--- Subject testing ---" << std::endl;
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
+		Fixed a;
+		Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	
+		std::cout << a << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << a << std::endl;
+		std::cout << a++ << std::endl;
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << Fixed::max( a, b ) << std::endl;
+		std::cout << std::endl;
+
+	}
+
+	{
+		std::cout << "--- Additional testing ---" << std::endl;
+
+		// Comparaisons
+		Fixed	a, b;
+
+		a = Fixed(15.4f);
+		b = Fixed(32.1f);
+
+		std::cout << "a: " << a << std::endl;
+		std::cout << "b: " << b << std::endl;
+		std::cout << "a < b: " << (a < b) << std::endl;
+		std::cout << "a > b: " << (a > b) << std::endl;
+		std::cout << "a == b: " << (a == b) << std::endl;
+		std::cout << "a != b: " << (a != b) << std::endl;
+		std::cout << std::endl;
+
+		// arithmetic
+
+		a = Fixed(15.4f);
+		b = Fixed(32.1f);
+
+		std::cout << "a: " << a << std::endl;
+		std::cout << "b: " << b << std::endl;
+		std::cout << "a + b = " << a + b << std::endl;
+
+	}
+
 	return (0);
 }
